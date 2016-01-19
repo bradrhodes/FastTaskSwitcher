7e8d56fbb660a32350c47db073eeaaf2ddf3bfa7
Brad Rhodes <bradr@iqmetrix.com>
2016-01-19 16:57:41 -0600

    Started work on the WPF project.

---
+++ FastTaskSwitcher/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using FastTaskSwitcher.Core;
+using FastTaskSwitcher.Core.ContextMenu;
+using Application = System.Windows.Application;
+
+namespace FastTaskSwitcher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void AppStartup(object sender, StartupEventArgs e)
+        {
+            using (var stp = new SysTrayIcon(new ContextMenuBuilder(), PopWindow, null, "FastTaskSwitcher"))
+            {
+                stp.Display();
+            }
+        }
+
+        private void PopWindow()
+        {
+            var searchWindow = new SearchWindow();
+            searchWindow.Show();
+        }
+    }
+
+    class ContextMenuBuilder : IContextMenuBuilder
+    {
+        public void BuildContextMenu(ContextMenuStrip menu)
+        {
+            menu.Items.Add(new AboutContextMenuItem().GetMenuItem());
+        }
+    }
+
+    public class AboutContextMenuItem : IContextMenuItem
+    {
+        public ToolStripMenuItem GetMenuItem()
+        {
+            return new ToolStripMenuItem("About", null, this.HandleClick);
+        }
+
+        public void HandleClick(object sender, EventArgs e)
+        {
+            new AboutBox().ShowDialog();
+        }
+    }
+}
---
+++ FastTaskSwitcher/SearchWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FastTaskSwitcher
+{
+    /// <summary>
+    /// Interaction logic for SearchWindow.xaml
+    /// </summary>
+    public partial class SearchWindow : Window
+    {
+        public SearchWindow()
+        {
+            InitializeComponent();
+            this.searchBox.Focus();
+
+            this.KeyUp += KeyCheck;
+        }
+
+        private void KeyCheck(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void searchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
